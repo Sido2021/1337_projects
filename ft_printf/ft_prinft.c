d369ae0417c9c51e783bdb560d893aaad7d62edc
Said Belyamani <sbelyama@c2r2p4.1337.ma>
2022-10-31 14:41:11 +0100

    some modification

---
+++ ft_printf/ft_prinft.c
@@ -0,0 +1,21 @@
+
+static int	get_vars_count(const char *s)
+{
+
+}
+
+int	ft_printf(const char *s, ...)
+{
+	va_list vars;
+
+	int vars_count = get_vars_count(s);
+	va_start(vars, s)
+}
+
+#include <stdio.h>
+
+int main(){
+	int n = 14;
+	ft_printf("",);
+	return (0);
+}
--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: sbelyama <sbelyama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 18:34:19 by sbelyama          #+#    #+#             */
-/*   Updated: 2022/10/20 22:32:31 by sbelyama         ###   ########.fr       */
+/*   Updated: 2022/10/31 13:39:43 by sbelyama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,11 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*s2;
+	size_t	i;
 
 	i = 0;
-	s2 = (unsigned char *) s;
 	while (i < n)
-	{
-		if (s2[i++] == (unsigned char )c)
-		{
-			if (!s2[0])
-				s2[i] = 0;
-			return (s2 + (i -1));
-		}
-	}
+		if (((unsigned char *)s)[i++] == (unsigned char )c)
+			return (((void *)s) + (i -1));
 	return (0);
 }
